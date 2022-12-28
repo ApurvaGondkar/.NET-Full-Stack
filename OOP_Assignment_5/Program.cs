1b4ee7779e335ad3496675fd3e2c2cc8bf2b8ec7
Gondkar <apurva.gondkar@capgemini.com>
2022-12-28 17:25:04 +0530

    Assignment OOP5 updated

--- OOP_Assignment_5/Program.cs
+++ OOP_Assignment_5/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections;
+
+
 
 namespace OOP_Assignment_5
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArrayList MyList = new ArrayList(3);
+            Console.WriteLine("Enter Information Of Employee");
+
+
+
+
+
+            int EmpId = Convert.ToInt32(Console.ReadLine());
+            string EmpName = Convert.ToString(Console.ReadLine());
+            double EmpSalary = Convert.ToDouble(Console.ReadLine());
+            MyList.Add(EmpId);
+            MyList.Add(EmpName);
+            MyList.Add(EmpSalary);
+            foreach (object obj in MyList)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
         }
     }
 }
