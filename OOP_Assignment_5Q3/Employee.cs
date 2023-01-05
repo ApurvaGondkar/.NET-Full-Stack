43e18404c2bc1e95bd8553e02503db2b35cc63d6
Gondkar <apurva.gondkar@capgemini.com>
2023-01-05 15:12:35 +0530

    Q.5-3 & Q.5-5 Combined

--- OOP_Assignment_5Q3/Employee.cs
+++ OOP_Assignment_5Q3/Employee.cs
@@ -1,48 +1,19 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-
-
-namespace Assignment_5_Q3
+namespace OOP_Assignment_5_3
 {
     class Employee
     {
-        public int ID { get; set; }
-
-
-
-        public string Name { get; set; }
-        public string Department { get; set; }
-
-
-
-        public string Joining_Date { get; set; }
-        public int Salary { get; set; }
+        public string name;
+        public Employee(string name)
+        {
+            this.name = name;
+        }
+        public override string ToString()
+        {
+            return (name);
+        }
     }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-
-
-namespace Assignment_5_Q3
-{
-    class Employee
-    {
-        public int ID { get; set; }
-
-
-
-        public string Name { get; set; }
-        public string Department { get; set; }
-
-
-
-        public string Joining_Date { get; set; }
-        public int Salary { get; set; }
-    }
->>>>>>> fa9fff6ce3abd128d0d6bdece9793e6d72a87386
 }
